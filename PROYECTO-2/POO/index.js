e987f28e9d1b09ad90d4268e034f3a3e40163637
Luis Marcelo Riquelme <luis.riquelme@correo.cl>
2024-08-26 17:04:35 -0400

    refactor: create POO class Encuesta and Preguntas

--- PROYECTO-2/POO/index.js
+++ PROYECTO-2/POO/index.js
@@ -1,1 +1,18 @@
-alert('Conectado POO');
+// alert('Conectado POO');
+
+// Creo la Clase Encuesta
+class Encuesta {
+    constructor(tituloEncuesta){ // Espero el argumento tituloEncuesta para guardar
+        this.titulo = tituloEncuesta; // asigno el tituloEncuesta a titulo
+        this.preguntas = []; //defino preguntas como un arrgelo vacio
+    }
+}
+
+// Creo la clase Preguntas donde guardare como texto la pregunta y las opciones las pasaré como un arreglo de respuestas posibles
+class Preguntas {
+    constructor(texto, opciones){
+        this.textoPregunta = texto; // Guardo la pregunta ingresada como texto
+        this.opcionesRespuesta = opciones;  // guardo el arreglo que traigo como variable opciones con las respuestas posibles
+        this.votos = new Array(opciones.length).fill(0); // Creo un nuevo arreglo del mismo largo de opciones y que todas las posiciones contengan un valor 0 para guardar los votos por opcion.
+    }
+}

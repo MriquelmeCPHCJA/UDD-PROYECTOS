26c0dc319713c5d2385dfaafc2123fc7a7e69c83
Marcelo Riquelme <mriquelme@correo.com>
2024-12-08 17:03:49 -0300

    init: router file

---
+++ PROYECTO-5/src/Router.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from 'react-router-dom';
+import { Layout } from "./components/Layout";
+import { Home } from "./pages/Home";
+import { MoviesList } from "./pages/MoviesList";
+import { MovieDetails } from "./pages/MovieDetails";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+
+const Router = createBrowserRouter([
+    {
+        path: "/", element: <Layout />,
+        children: [
+            { path: "/", element: <Home /> },
+            { path: "/movies", element: <MoviesList /> },
+            { path: "/movies/:id", element: <MovieDetails /> },
+            { path: "/about", element: <About /> },
+            { path: "/contact", element: <Contact /> }
+        ]
+    }
+]);

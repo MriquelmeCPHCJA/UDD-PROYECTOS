87cf967066516ea055644ddafe6f7b44fc172019
Luis Marcelo Riquelme <luis.riquelme@correo.cl>
2024-12-10 10:40:51 -0300

    refactor: add pagination in MoviList file

--- PROYECTO-5/src/pages/MoviesList.jsx
+++ PROYECTO-5/src/pages/MoviesList.jsx
@@ -1,22 +1,42 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { CardContent, CardMedia, Typography, CircularProgress, Card, CardActionArea, Grid2 } from '@mui/material';
+import KeyboardDoubleArrowLeftOutlinedIcon from '@mui/icons-material/KeyboardDoubleArrowLeftOutlined';
+import KeyboardDoubleArrowRightOutlinedIcon from '@mui/icons-material/KeyboardDoubleArrowRightOutlined';
+import KeyboardArrowLeftOutlinedIcon from '@mui/icons-material/KeyboardArrowLeftOutlined';
+import KeyboardArrowRightOutlinedIcon from '@mui/icons-material/KeyboardArrowRightOutlined';
+import { CardContent, 
+         CardMedia, 
+         Typography, 
+         CircularProgress, 
+         Card, 
+         CardActionArea, 
+         Grid2, 
+         Container,
+         ButtonGroup,
+         Button,
+         Pagination } from '@mui/material';
 
 import '../components/moviesList.css'
 
-
 const API_KEY = 'f76d94834aca1cb630bca53ecf4e4c56'
-const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
 const IMG_POSTER = 'https://image.tmdb.org/t/p/original'
 
 export const MoviesList = () => {
 
   const [movies, setMovies] = useState([])
   const [loading, setLoading] = useState(true)
+  const [page, setPage] = useState(1)
+  const [totalPages, setTotalPages] = useState(20)
+  
+  const changePage = (page) => {
+    setPage(page)
+  }
 
-  useEffect(() => {
+  const fetchMovies = () => {
 
     setLoading(true)
+
+    const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=${page}`
 
     fetch(API_URL)
       .then(response => response.json())
@@ -25,8 +45,11 @@ export const MoviesList = () => {
         console.log('Error de Fetch: ', error)
       })
       .finally(() => setLoading(false))
-  }, []);
+  }
 
+  useEffect(() => {
+    fetchMovies()
+  }, [page])
 
   if (loading) {
     return (
@@ -41,41 +64,61 @@ export const MoviesList = () => {
   }
 
     return (
-      <Grid2 container spacing={4} sx={{
-        padding: '20px', 
-        backgroundColor: 'black', 
-        color: 'white',
-        alignItems: "center",
-        justifyContent: "center"}}>
-        {
-        movies.map(movie => (
-          <Grid2 item xs={12} md={6} lg={4} key={movie.id}>
-            <Card>
-              <CardActionArea
-                component={Link}
-                to={`/movie/${movie.title}`}
-                state={{movie}}
-              >
-                <CardMedia
-                  component="img"
-                  height="420"
+      <>
+        <Grid2 container spacing={4} sx={{
+          padding: '20px', 
+          backgroundColor: 'black', 
+          color: 'white',
+          alignItems: "center",
+          justifyContent: "center"}}>
+          {
+          movies.map(movie => (
+            <Grid2 item xs={12} md={6} lg={4} key={movie.id}>
+              <Card sx={{backgroundColor: "black"}}>
+                <CardActionArea
+                  component={Link}
+                  to={`/movie/${movie.title}`}
+                  state={{movie}}
+                >
+                  <CardMedia
+                    component="img"
+                    height="420"
+                    image={`${IMG_POSTER}/${movie.poster_path}`}
+                    alt={movie.title}
+                    sx={{objectFit: 'contain',  borderRadius: '15px'}}
+                  />
 
-                  image={`${IMG_POSTER}/${movie.poster_path}`}
-                  alt={movie.title}
-                  sx={{objectFit: 'contain'}}
-                />
-                {/* <CardContent sx={{backgroundColor: 'black'}}>
-                  <Typography gutterBottom variant="h6" sx={{color: 'white'}}>
-                    {movie.title}
-                  </Typography>
+                  {/* <CardContent sx={{backgroundColor: 'black'}}>
+                    <Typography gutterBottom variant="h6" sx={{color: 'white'}}>
+                      {movie.title}
+                    </Typography>
 
-                </CardContent> */}
-              </CardActionArea>
-            </Card>
+                  </CardContent> */}
+                </CardActionArea>
+              </Card>
+            </Grid2>
+          ))
+          }
+
+        </Grid2 >
+        <Grid2 container spacing={4} sx={{
+          padding: '20px', 
+          backgroundColor: 'black', 
+          color: 'white',
+          alignItems: "center",
+          justifyContent: "center"}}>
+
+              <Pagination
+                      color='primary'
+                      count={totalPages}
+                      page={page}
+                      onChange={(event, newPage) => {
+                        changePage(newPage);
+                      }}
+                      sx={{backgroundColor: 'white', borderRadius: '15px'}}
+              />
           </Grid2>
-        ))
-        }
-      </Grid2>
+      </>
     )
 
 }
